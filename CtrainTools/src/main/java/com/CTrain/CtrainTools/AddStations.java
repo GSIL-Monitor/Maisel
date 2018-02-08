683c83c2b964592ecfc1ded371bf125580334d51
fiona_66 <lulu_fiona@163.com>
2018-02-08 20:15:46 +0800

    add chromedriver in ./src/main/resources/drivers

--- CtrainTools/src/main/java/com/CTrain/CtrainTools/AddStations.java
+++ CtrainTools/src/main/java/com/CTrain/CtrainTools/AddStations.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddStations {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","/Users/yanglu/Downloads/selenium-java-3.8.1/chromedriver");
+        System.setProperty("webdriver.chrome.driver","./src/main/resources/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://admin.phenix.train.uat.qa.nt.ctripcorp.com/phenix-admin/Login");
         driver.findElement(By.id("txt_account")).sendKeys("test_user");
@@ -18,13 +18,13 @@ public class AddStations {
             System.out.println(driver.getPageSource());
             // 跳转至测试环境刷数据页
             driver.findElement(By.xpath("//a[@href='/phenix-admin/SystemManage/SetTestData/Index']")).click();
-            driver.wait(2000);
+            driver.wait(3000);
             // 切换为右侧页面，无此句无法定位后面元素
             driver.switchTo().frame("iframecygj-01");
-            driver.findElement(By.id("from")).sendKeys("上海");
-            driver.findElement(By.id("to")).sendKeys("昆山");
+            driver.findElement(By.id("from")).sendKeys("上海虹桥");
+            driver.findElement(By.id("to")).sendKeys("重庆北");
             driver.findElement(By.id("departDate")).clear();//清除默认的当前日期
-            driver.findElement(By.id("departDate")).sendKeys("20180227");
+            driver.findElement(By.id("departDate")).sendKeys("20180222");
             //刷入站站车次数据
             driver.findElement(By.id("btn_set")).click();
             driver.wait(2000);
